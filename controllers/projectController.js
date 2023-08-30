badc2868207e1fb1fb02051cee4d2334b129cc07
arnaudchacon <arnaudchacon@gmail.com>
2023-08-30 17:32:40 +0900

    Added file upload functionality to project creation

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,18 +1,31 @@
 const Project = require('../models/project');
 
 exports.createProject = async (req, res) => {
-  try {
-    const { name, description } = req.body;
-    const project = await Project.create({
-      name,
-      description,
-      userId: req.user.userId // from JWT
-    });
-    res.status(201).json({ message: 'Project created successfully', projectId: project.id });
-  } catch (error) {
-    res.status(500).json({ message: 'Error creating project', error });
-  }
-};
+    try {
+      const { name, description } = req.body;
+      const userId = req.user.userId;  // from JWT
+      const floorPlan = req.file.path;  // New line to handle the uploaded file
+  
+      const project = await Project.create({
+        name,
+        description,
+        userId,
+        floorPlan  // New line to save the file path in the database
+      });
+  
+      res.status(201).json({
+        message: 'Project created successfully',
+        projectId: project.id,
+        floorPlan: floorPlan  // Optionally return the file path
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: 'Error creating project',
+        error
+      });
+    }
+  };
+  
 
 exports.getProjects = async (req, res) => {
   try {
--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const projectController = require('../controllers/projectController');
 const auth = require('../middleware/authMiddleware');
+const upload = require('../middleware/uploadMiddleware');
 
-router.post('/create', auth, projectController.createProject);
+router.post('/create', auth, upload.single('floorPlan'), projectController.createProject);
 router.get('/list', auth, projectController.getProjects);  // Changed this line
 
 module.exports = router;
