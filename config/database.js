e67440f9fbbf4d3bf6e90ffdd6c4c05b9243b108
arnaudchacon <arnaudchacon@gmail.com>
2023-09-19 22:55:01 +0900

    Update database configurations for Heroku deployment

--- config/database.js
+++ config/database.js
@@ -2,9 +2,11 @@ const { Sequelize } = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require('./config.json')[env];
 
-const sequelize = new Sequelize(config.database, config.username, config.password, {
-  host: config.host,
-  dialect: config.dialect
-});
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 module.exports = sequelize;
